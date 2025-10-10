fa7cf1aef02cc0727bebca5ff8c34b5b9a41b6d5
Tjones <taouris.jones@gmail.com>
2025-10-10 14:19:08 -0400

    commit

--- src/main/java/com/pluralsight/ALApp.java
+++ src/main/java/com/pluralsight/ALApp.java
@@ -1,26 +1,50 @@
 package com.pluralsight;
 
 import java.sql.SQLOutput;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ALApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        homeScreen();
+        homeScreen(scan);
 
 
     }
-    public static void homeScreen(){
-        boolean appOpen =true;
+    public static void homeScreen(Scanner scan){
+        boolean appOpen = true;
 
-        System.out.println("Ledgar Has Been Accessed");
         while (appOpen){
+        System.out.println("Welcome To Your Banking App");
+            System.out.println();
             System.out.println("D) Add Deposit");
             System.out.println("P) Make Payment (Debit)");
             System.out.println("L) Ledger");
             System.out.println("X) Exit");
-            String selection = scanner.nextChar();
+            System.out.println("PLease Make your Selection");
+            String choice  = scan.nextLine().toUpperCase(Locale.ROOT);
 
+            switch (choice) {
+                case "D":
+               //     addDeoposit(scan);
+                    break;
+
+                case "P":
+                  //  makePayment(scan);
+                    break;
+
+                case "L":
+               //     showLedger(scan);
+
+                case "X":
+
+
+
+            }
+
+            if (choice.equals("X")){
+                appOpen = false;
+            }
         }
     }
 }
